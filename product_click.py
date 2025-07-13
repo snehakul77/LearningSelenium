fd6508a3420ee78f107962f16990697b584ccd67
Sneha kulkarni <snehakrishna.kulkarni@outlook.com>
2025-07-13 14:07:57 +0200

    Update product_click.py
    xpath updated

--- product_click.py
+++ product_click.py
@@ -14,7 +14,7 @@ search_box.send_keys("iphone")
 search_box.submit()
 
 wait = WebDriverWait(driver, 10)
-first_product = wait.until(EC.element_to_be_clickable((By.XPATH, "(//div[@data-component-type='s-search-result'])[1]//h2/a")))
+first_product = wait.until(EC.element_to_be_clickable((By.XPATH, "(//div[@data-component-type='s-search-result'])[1]//img[@class='s-image']")))
 first_product.click()
 
 wait.until(EC.presence_of_element_located((By.ID, "productTitle")))
